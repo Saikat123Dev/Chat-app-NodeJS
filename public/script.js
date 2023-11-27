93958a73e3a7a57a28a47b585c7303f7ec7b1658
Saikat123Dev <berasaikat729@gmail.com>
2023-11-27 20:08:11 +0530

    Added coes

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 var socket = io();
-
 let btn = document.getElementById('btn');
+let inputMsg = document.getElementById('newmsg');
+let msglist = document.getElementById('msgList');
+
 btn.onclick = function exec(){
-    socket.emit('from_client');
+    socket.emit('msg_send',{
+        msg: inputMsg.value
+    });
 }
 
-
-
-socket.on('from_server', ()=>{
-    console.log("Colle ted a new event from server");
-    const div = document.createElement('div');
-    div.innerText = 'New event from server';
-    console.log(div)
-    document.body.appendChild(div);
-});
+socket.on('msg_rcvd', (data)=>{
+    let limsg = document.createElement('li');
+    limsg.innerText = data.msg;
+    msglist.appendChild(limsg);
+})
